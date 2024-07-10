56bd3a6e347d50088ad8587cb7cdb1b33e9dacaf
PabloAOjedaG <p.ojeda@uniandes.edu.co>
2024-07-10 10:20:55 -0500

    Modified the Ituran code to download the odometer form as well.

--- forms/ituranForm.py
+++ forms/ituranForm.py
@@ -12,7 +12,7 @@ import os
 
 archivoIturan1 = os.getcwd() + "\\outputIturan\\report.csv"
 archivoIturan2 = os.getcwd() + "\\outputIturan\\report (1).csv"
-
+archivoIturan3 = os.getcwd() + "\\outputIturan\\report (2).csv"
 
 opcionesNavegador = webdriver.ChromeOptions()
 lugarDescargasIturan = os.getcwd() + r"\outputIturan"
@@ -117,13 +117,44 @@ WebDriverWait(driver,50).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".sl
 driver.find_element(By.CSS_SELECTOR,".slidingDiv > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)").click()
 time.sleep(2)
 
+# Verificar si puede salirse de la descarga.
+while os.path.isfile(archivoIturan2) == True:
+    break
+else:
+    time.sleep(1)
+
+####################################
+#### Descargar Distancia Diaria ####
+####################################
+
+
+driver.switch_to.default_content()
+
+# Seleccionar "Distancia diaria de vehículos"
+WebDriverWait(driver,50).until(EC.presence_of_element_located((By.XPATH,"/html/body/form/div[4]/div/div/div/table/tbody/tr/td[1]/div/table/tbody/tr[2]/td/div/ul/li[1]/div/ul/li[5]")))
+time.sleep(1)
+driver.find_element(By.XPATH,"/html/body/form/div[4]/div/div/div/table/tbody/tr/td[1]/div/table/tbody/tr[2]/td/div/ul/li[1]/div/ul/li[5]").click()
+time.sleep(1)
+
+# Oprimir botón exportar.
+WebDriverWait(driver,50).until(EC.presence_of_element_located((By.NAME, "RadWndCreteria")))
+driver.switch_to.frame(driver.find_element(By.NAME, "RadWndCreteria"))
+WebDriverWait(driver,50).until(EC.presence_of_element_located((By.ID, "imgExportArrow")))
+time.sleep(1)
+driver.find_element(By.ID,"imgExportArrow").click()
+
+# Descargar en formato CSV.
+WebDriverWait(driver,50).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".slidingDiv > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)")))
+driver.find_element(By.CSS_SELECTOR,".slidingDiv > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)").click()
+time.sleep(2)
+
 
 ####################################
 ####### Cierre del Webdriver #######
 ####################################
 
 
-while os.path.isfile(archivoIturan2) == True:
+while os.path.isfile(archivoIturan3) == True:
     time.sleep(2)
     driver.quit()
     break
