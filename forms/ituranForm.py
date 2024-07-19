df721d461c7b18ba9fe16a22896147806a611c93
PabloAOjedaG <p.ojeda@uniandes.edu.co>
2024-07-18 21:32:53 -0500

    Updated to fit next test.

--- forms/ituranForm.py
+++ forms/ituranForm.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
-#from util.funcionalidadVehicular import Navegador
 
 # class IturanDatos(Navegador):
 #    def __init__(self, opcionesNavegador, opcionDescarga):
