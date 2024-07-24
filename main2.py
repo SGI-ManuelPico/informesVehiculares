7739ff0483f2caf3e3089ca1d3bcab490e7a7bed
Manuelpd21 <manuelpico121314@gmail.com>
2024-07-23 20:11:24 -0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -9,25 +9,25 @@ from util.correosVehiculares import CorreosVehiculares
 from persistence.archivoExcel import FuncionalidadExcel
 from persistence.extraerExcel import Extracciones
 from persistence.scriptMySQL import actualizarKilometraje, actualizarSeguimientoSQL, actualizarInfractoresSQL
-from persistence.estadoPlataforma import actualizarEstado, verificarEstado, logError, resetEstados
+from persistence.estadoPlataforma import EstadoPlataforma
 
 ### Como estas funciones solo se van a ejecutar en el main, las defino acá. ###
 
 def ejecutarTareaRPA(plataforma, funcionRPA):
     try:
-        actualizarEstado(plataforma, 'En ejecucion')
+        EstadoPlataforma.actualizarEstado(plataforma, 'En ejecucion')
         resultado = funcionRPA()
-        actualizarEstado(plataforma, 'Finalizado')
+        EstadoPlataforma.actualizarEstado(plataforma, 'Finalizado')
         return resultado
     except Exception as e:
         print(f"Hubo un error en el acceso por el internet para ingresar a {plataforma}.")
-        actualizarEstado(plataforma, 'Error')
+        EstadoPlataforma.actualizarEstado(plataforma, 'Error')
         CorreosVehiculares.enviarCorreoPlataforma(plataforma)
 
 # Verifica si hay alguna plataforma que tenga estado 'error' en la tabla estadoPlataforma. Si los hay, retorna True, si no, retorna False.
 
 def checkErrores():
-    status = verificarEstado()
+    status = EstadoPlataforma.verificarEstado()
     for plataforma, estado in status:
         if estado == 'Error':
             return True
@@ -36,7 +36,7 @@ def checkErrores():
 # Si hay una plataforma con estado 'error' en la tabla estadoPlataforma, ejecuta el RPA para esa plataforma específica.
 
 def retryErrores(plataformas, resultados):
-    status = verificarEstado()
+    status = EstadoPlataforma.verificarEstado()
     for plataforma, estado in status:
         if estado == 'Error':
             resultados[plataforma] = ejecutarTareaRPA(plataforma, dict(plataformas)[plataforma])
@@ -77,16 +77,16 @@ def main():
             retryErrores(plataformas, resultados)
 
     # Verificar estados finales y registrar errores en la tabla 'error'
-    estadoFinal = verificarEstado()
+    estadoFinal = EstadoPlataforma.verificarEstado()
     existe_error = False
     for plataforma, estado in estadoFinal:
         if estado == 'Error':
-            logError(plataforma)
+            EstadoPlataforma.logError(plataforma)
             existe_error = True
 
     # Si no hay errores en las plataformas, reseteamos el estado de todas las plataformas en la tabla 'estadoPlataforma'
     if not existe_error:
-        resetEstados()
+        EstadoPlataforma.resetEstados()
 
     # Rutas.
     archivoIturan1, archivoIturan2, archivoIturan3 = resultados.get('Ituran', (None, None, None))
